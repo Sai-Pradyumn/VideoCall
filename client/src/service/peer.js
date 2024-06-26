3c92e487b0e075d6a2c5c6b286b76cb16689d42d
Sai-Pradyumn <saiprady147@gmail.com>
2024-06-26 21:47:29 +0530

    added track in peer.js

--- client/src/service/peer.js
+++ client/src/service/peer.js
@@ -1,42 +1,55 @@
+// peer.js
+
 class PeerService {
-    constructor() {
-      if (!this.peer) {
-        this.peer = new RTCPeerConnection({
-          iceServers: [
-            {
-              urls: [
-                "stun:stun.l.google.com:19302",
-                "stun:global.stun.twilio.com:3478",
-              ],
-            },
+  constructor() {
+    this.peer = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:global.stun.twilio.com:3478",
           ],
-        });
+        },
+      ],
+    });
+
+    // Event listener for incoming tracks
+    this.peer.addEventListener("track", (event) => {
+      if (event.streams && event.streams[0]) {
+        this.onTrack(event.streams[0]);
       }
-    }
-  
-    async getAnswer(offer) {
-      if (this.peer) {
-        await this.peer.setRemoteDescription(offer);
-        const ans = await this.peer.createAnswer();
-        await this.peer.setLocalDescription(new RTCSessionDescription(ans));
-        return ans;
-      }
-    }
-  
-    async setLocalDescription(ans) {
-      if (this.peer) {
-        await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
-      }
-    }
-  
-    async getOffer() {
-      if (this.peer) {
-        const offer = await this.peer.createOffer();
-        await this.peer.setLocalDescription(new RTCSessionDescription(offer));
-        return offer;
-      }
-    }
+    });
   }
-  
-  export default new PeerService();
-  
+
+  async getOffer() {
+    const offer = await this.peer.createOffer();
+    await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+    return offer;
+  }
+
+  async getAnswer(offer) {
+    await this.peer.setRemoteDescription(new RTCSessionDescription(offer));
+    const ans = await this.peer.createAnswer();
+    await this.peer.setLocalDescription(new RTCSessionDescription(ans));
+    return ans;
+  }
+
+  async setLocalDescription(ans) {
+    await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
+  }
+
+  // Method to add track to peer connection
+  addTrack(track, stream) {
+    const sender = this.peer.addTrack(track, stream);
+    return sender;
+  }
+
+  // Event handler for incoming tracks
+  onTrack(stream) {
+    // Handle incoming tracks here
+    console.log("Received remote track:", stream);
+    // You can set this stream to the state or handle it as needed
+  }
+}
+
+export default new PeerService();
