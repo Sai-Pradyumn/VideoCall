d53c0a46cc4a8d9f08803a5b73a0d1ea51e142d4
Sai-Pradyumn <saiprady147@gmail.com>
2024-06-26 19:20:47 +0530

    added-backend-link

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io(), []);  // Connect to the same origin
+  const socket = useMemo(() => io("https://videocall-kai5.onrender.com"), []);  // Connect to the same origin
 
   return (
     <SocketContext.Provider value={socket}>
